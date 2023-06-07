8a9c87f248379d3a318cefe15b9947606bcf0244
jake-s2021 <53814560+jake-s2021@users.noreply.github.com>
2023-06-06 19:10:25 -0500

    initial upload

---
+++ src/button_handlers.h
@@ -0,0 +1,91 @@
+#define RIGHT READ_PA3_IN
+#define LEFT READ_PA2_IN
+
+
+unsigned char last_pressed = 2;
+
+/*
+IN:
+UP = PA0
+DOWN = PA1
+LEFT = PA2
+RIGHT = PA3
+
+
+OUT:
+UP = PA4
+DOWN = PA5
+LEFT = PA6
+RIGHT = PA7
+
+
+TOGGLE:
+LR = PB0
+UD = PB1
+*/
+
+
+unsigned char handle_up_priority(){
+    unsigned char A = (READ_PA0_IN);
+    unsigned char C = (PORTA & (1<<PA4));
+
+    return A != C;
+
+}
+unsigned char handle_up_neutral(){
+    unsigned char A = (READ_PA0_IN);
+    unsigned char B = (PORTA & (1<<PA4));
+    unsigned char C = (READ_PA1_IN);
+    unsigned char D =  (PORTA & (1<<PA5));
+
+    return (!A && B && !C || !A && B && !C || A && !B && !C || B && C && A || B && C && D);
+}
+unsigned char handle_down(){
+    unsigned char A = (READ_PA0_IN);
+    unsigned char B = (PORTA & (1<<PA4));
+    unsigned char C = (READ_PA1_IN);
+    unsigned char D =  (PORTA & (1<<PA5));
+
+    return (!A && C && !D || !C && D && !B || !C && D && !A || A && D && B || A && D && C);
+
+}
+unsigned char handle_left_neutral(){
+    unsigned char A = (READ_PA2_IN);
+    unsigned char B = (PORTA & (1<<PA6));
+    unsigned char C = (READ_PA3_IN);
+    unsigned char D =  (PORTA & (1<<PA7));
+
+    return (!A && B && !C || !A && B && !C || A && !B && !C || B && C && A || B && C && D);
+
+}
+unsigned char handle_right_neutral(){
+    unsigned char A = (READ_PA2_IN);
+    unsigned char B = (PORTA & (1<<PA6));
+    unsigned char C = (READ_PA3_IN);
+    unsigned char D =  (PORTA & (1<<PA7));
+
+    return (!A && C && !D || !C && D && !B || !C && D && !A || A && D && B || A && D && C);
+
+}
+unsigned char handle_left_major(){
+
+    return (!(PORTA & (1<<PA6)) && (LEFT && !RIGHT || LEFT && last_pressed == 1) ) 
+    || ((PORTA & (1<<PA6)) && RIGHT) || ((PORTA & (1<<PA6) && !LEFT)); 
+
+}
+unsigned char handle_right_major(){
+
+    return (!(PORTA & (1<<PA7)) && (RIGHT && !LEFT || RIGHT && last_pressed == 0) )
+    || (PORTA & (1<<PA7) && LEFT) || ((PORTA & (1<<PA7) && !RIGHT));
+
+}
+void handle_last_pressed(){
+
+    if(LEFT && !RIGHT){
+        last_pressed = 0;
+    }
+    else if(RIGHT && !LEFT){
+        last_pressed = 1;
+    }
+
+}
---
+++ src/socd.c
@@ -0,0 +1,74 @@
+#include<avr/io.h>
+#include "attiny84.h"
+#include "button_handlers.h"
+
+/*
+IN:
+UP = PA0
+DOWN = PA1
+LEFT = PA2
+RIGHT = PA3
+
+
+OUT:
+UP = PA4
+DOWN = PA5
+LEFT = PA6
+RIGHT = PA7
+
+
+TOGGLE:
+LR = PB0
+UD = PB1
+*/
+
+int main(){
+    SET_PA0_INPUT;
+    SET_PA1_INPUT;
+    SET_PA2_INPUT;
+    SET_PA3_INPUT;
+    SET_PB0_INPUT;
+    SET_PB1_INPUT;
+    SET_PA0_PULLUP;
+    SET_PA1_PULLUP;
+    SET_PA2_PULLUP;
+    SET_PA3_PULLUP;
+    SET_PB0_PULLUP;
+    SET_PB1_PULLUP;
+
+
+    SET_PA4_OUTPUT;
+    SET_PA5_OUTPUT;
+    SET_PA6_OUTPUT;
+    SET_PA7_OUTPUT;
+
+    while(1){
+        while(READ_PB0_IN && READ_PB1_IN){
+            PORTA ^= (handle_up_priority() << PA4);
+            PORTA ^= (handle_down() << PA5);
+            PORTA ^= (handle_left_major() << PA6);
+            PORTA ^= (handle_right_major() << PA7);
+            handle_last_pressed();
+        }
+        while(!READ_PB0_IN && READ_PB1_IN){
+            PORTA ^= (handle_up_priority() << PA4);
+            PORTA ^= (handle_down() << PA5);
+            PORTA ^= (handle_left_neutral() << PA6);
+            PORTA ^= (handle_right_neutral() << PA7);
+    
+        }
+        while(READ_PB0_IN && !READ_PB1_IN){
+            PORTA ^= (handle_up_neutral() << PA4);
+            PORTA ^= (handle_down() << PA5);
+            PORTA ^= (handle_left_major() << PA6);
+            PORTA ^= (handle_right_major() << PA7);
+            handle_last_pressed();
+        }
+        while(!READ_PB0_IN && !READ_PB1_IN){
+            PORTA ^= (handle_up_neutral() << PA4);
+            PORTA ^= (handle_down() << PA5);
+            PORTA ^= (handle_left_neutral() << PA6);
+            PORTA ^= (handle_right_neutral() << PA7);
+        }
+    }
+}
